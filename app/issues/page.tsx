5b99be43a5ff148fcfb1cf29aac40a75fca05b8f
Fnu Adnan <fnu.adnan12@gmail.com>
2023-11-14 23:25:45 -0600

    Show issue details

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -28,7 +28,7 @@ const New = async () => {
           {issues.map((issue) => (
             <Table.Row key={issue.id}>
               <Table.Cell>
-                {issue.title}
+                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
                 <div className="block md:hidden">
                   <IssueStatusBadge status={issue.status} />
                 </div>
