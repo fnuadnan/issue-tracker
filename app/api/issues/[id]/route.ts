62c8f02a34bbb08fe5d36d070ce056332e61e26b
Fnu Adnan <fnu.adnan12@gmail.com>
2023-11-17 00:23:36 -0600

    Build an API for deleting issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -27,3 +27,15 @@ export async function PATCH(request: NextRequest, {params}: Props) {
         });
     return NextResponse.json(updatedIssue)
 }
+
+export async function DELETE(request: NextRequest, {params}: Props) {
+    const issue = await prisma.issue.findUnique({
+        where: {id: parseInt(params.id)}
+    })
+    if (!issue) 
+        return NextResponse.json({error: 'Invalid issue'}, {status: 404})
+    await prisma.issue.delete({
+        where: {id: issue.id}
+    });
+    return NextResponse.json({});
+}
