bfd0788357b8dbb28926496bb8e24e884a406e8f
Fnu Adnan <fnu.adnan12@gmail.com>
2023-11-18 21:51:02 -0600

    Add the login and logout links

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,39 +1,47 @@
-'use client'
+"use client";
 
-import Link from 'next/link'
-import React from 'react'
-import {AiFillBug} from 'react-icons/ai'
-import classNames from 'classnames'
-import { usePathname } from 'next/navigation'
+import Link from "next/link";
+import React from "react";
+import { AiFillBug } from "react-icons/ai";
+import classNames from "classnames";
+import { usePathname } from "next/navigation";
+import { useSession } from "next-auth/react";
+import { Box } from "@radix-ui/themes";
 
 const NavBar = () => {
+  const { status, data: session } = useSession();
   const links = [
     { label: "Dashboard", href: "/" },
-    { label: "Issues", href: "/issues"},
+    { label: "Issues", href: "/issues" },
   ];
   const currentPath = usePathname();
   return (
-    
-    <nav className='flex text-black space-x-6 border-b mb-5 px-5 h-14 items-center'>
-        <Link href='/'><AiFillBug /></Link>
-        <ul className="flex space-x-6">
-      {links.map((link) => (
-        <li key={link.href}>
-          <Link
-            className={classNames({
-              "text-zinc-900": link.href === currentPath,
-              "text-zinc-500": link.href !== currentPath,
-              "hover:text-zinc-800  transition-colors": true
-            })}
-            href={link.href}
-          >
-            {link.label}
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <nav className="flex text-black space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <li key={link.href}>
+            <Link
+              className={classNames({
+                "text-zinc-900": link.href === currentPath,
+                "text-zinc-500": link.href !== currentPath,
+                "hover:text-zinc-800  transition-colors": true,
+              })}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <Box>
+        {status == 'authenticated' && <Link href='/api/auth/signout'>Log Out</Link>}
+        {status == 'unauthenticated' && <Link href='/api/auth/signin'>Login</Link>}
+      </Box>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar 
+export default NavBar;
