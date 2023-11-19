15926f957367acf1b3bbef3908d8c78aff2c2bea
Fnu Adnan <fnu.adnan12@gmail.com>
2023-11-18 22:33:27 -0600

    Add a loading Skeleton

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -14,6 +14,8 @@ import {
   Flex,
   Text,
 } from "@radix-ui/themes";
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 const NavBar = () => {
   return (
@@ -62,7 +64,7 @@ const NavLinks = () => {
 const AuthStatus = () => {
   const { status, data: session } = useSession();
 
-  if (status === "loading") return null;
+  if (status === "loading") return <Skeleton width='3rem'/>;
   if (status === "unauthenticated")
     return <Link className="nav-link" href="/api/auth/signin">Login</Link>;
 
