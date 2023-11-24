e1d04a3f663b078d892b6f7d9e8c6be3bb7fefde
Fnu Adnan <fnu.adnan12@gmail.com>
2023-11-23 23:34:22 -0600

    Optimize performance using react cache

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,22 +1,27 @@
+import authOptions from "@/app/auth/authOptions";
 import prisma from "@/prisma/client";
 import { Box, Flex, Grid } from "@radix-ui/themes";
+import { getServerSession } from "next-auth";
 import { notFound } from "next/navigation";
+import { cache } from "react";
+import AssigneeSelect from "./AssigneeSelect";
+import DeleteIssueButton from "./DeleteIssueButton";
 import EditIssueButton from "./EditIssueButton";
 import IssueDetails from "./IssueDetails";
-import DeleteIssueButton from "./DeleteIssueButton";
-import { getServerSession } from "next-auth";
-import authOptions from "@/app/auth/authOptions";
-import AssigneeSelect from "./AssigneeSelect";
 
 interface Props {
   params: { id: string };
 }
 
+const fetchUser = cache((issueId: number) =>
+  prisma.issue.findUnique({ where: { id: issueId } })
+);
+
 const IssueDetailPage = async ({ params }: Props) => {
   const session = await getServerSession(authOptions)
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id) },
-  });
+
+  const issue = await fetchUser(parseInt(params.id))
+
   if (!issue) notFound();
 
   return (
@@ -24,24 +29,26 @@ const IssueDetailPage = async ({ params }: Props) => {
       <Box className="md:col-span-4">
         <IssueDetails issue={issue} />
       </Box>
-      {session && <Box>
-        <Flex direction='column' gap='4'>
-          <AssigneeSelect issue={issue}/>
-          <EditIssueButton issueId={issue.id} />
-          <DeleteIssueButton issueId={issue.id} />
-        </Flex>
-      </Box>}
+      {session && (
+        <Box>
+          <Flex direction="column" gap="4">
+            <AssigneeSelect issue={issue} />
+            <EditIssueButton issueId={issue.id} />
+            <DeleteIssueButton issueId={issue.id} />
+          </Flex>
+        </Box>
+      )}
     </Grid>
   );
 };
 
-export async function generateMetadata({params}: Props) {
-  const issue = await prisma.issue.findUnique({where: {id: parseInt(params.id)}})
+export async function generateMetadata({ params }: Props) {
+  const issue = await fetchUser(parseInt(params.id));
 
   return {
     title: issue?.title,
-    description: "Details of issue" + issue?.id
-  }
+    description: "Details of issue" + issue?.id,
+  };
 }
 
 export default IssueDetailPage;
