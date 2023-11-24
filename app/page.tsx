53cd6d5093b89d2aed358fcebb7c23b59236c35b
Fnu Adnan <fnu.adnan12@gmail.com>
2023-11-23 19:06:13 -0600

    Build the issueSummary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import LatestIssues from "./LatestIssues";
-import Pagination from "./components/Pagination";
+import prisma from "@/prisma/client";
+import IssueSummary from "./IssueSummary";
 
+export default async function Home() {
+  const open = await prisma.issue.count({ where: { status: "OPEN" } });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
 
-export default function Home() {
-  return (
-    <LatestIssues/>
-  );
+  return <IssueSummary open={open} inProgress={inProgress} closed={closed} />;
 }
