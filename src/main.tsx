b73a317e4b0d4934fd62648a6e8a9c872530aa8e
Fnu Adnan <129648022+fnuadnan@users.noreply.github.com>
2024-06-09 15:34:08 -0500

    Set up Radix UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+import { Theme } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
@@ -6,6 +8,8 @@ import router from "./routes";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Theme>
+      <RouterProvider router={router} />
+    </Theme>
   </React.StrictMode>
 );
