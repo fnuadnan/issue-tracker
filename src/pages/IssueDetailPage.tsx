b7cb4cc0f05f5defde3428d2a2ea52f8d18520d3
Fnu Adnan <129648022+fnuadnan@users.noreply.github.com>
2024-06-14 18:17:53 -0500

    Add the edit button

--- src/pages/IssueDetailPage.tsx
+++ src/pages/IssueDetailPage.tsx
@@ -1,5 +1,7 @@
-import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import { Pencil2Icon } from "@radix-ui/react-icons";
+import { Box, Button, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes";
 import ReactMarkdown from "react-markdown";
+import { Link } from "react-router-dom";
 import IssueStatusBadge from "../components/IssueStatusBadge";
 import LoadingIssueDetailPage from "../components/LoadingIssueDetailPage";
 import useFetchIssueDetail from "../hooks/useFetchIssueDetail";
@@ -16,16 +18,24 @@ const IssueDetailPage = () => {
   }
 
   return (
-    <div>
-      <Heading>{issue.title}</Heading>
-      <Flex gap="3" my="2">
-        <IssueStatusBadge status={issue.status} />
-        <Text>{new Date(issue.createdAt).toDateString()}</Text>
-      </Flex>
-      <Card className="prose" mt="4">
-        <ReactMarkdown>{issue.description}</ReactMarkdown>
-      </Card>
-    </div>
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Box>
+        <Heading>{issue.title}</Heading>
+        <Flex gap="3" my="2">
+          <IssueStatusBadge status={issue.status} />
+          <Text>{new Date(issue.createdAt).toDateString()}</Text>
+        </Flex>
+        <Card className="prose" mt="4">
+          <ReactMarkdown>{issue.description}</ReactMarkdown>
+        </Card>
+      </Box>
+      <Box>
+        <Button>
+          <Pencil2Icon />
+          <Link to={`/issues/${issue.id}/edit`}>Edit Issue</Link>
+        </Button>
+      </Box>
+    </Grid>
   );
 };
 
