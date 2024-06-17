53189c3e8e099f89e08225a40300ba5cbb27fad1
Fnu Adnan <129648022+fnuadnan@users.noreply.github.com>
2024-06-16 23:06:53 -0500

    Change the layout of the navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,6 @@
+import { Box, Container, Flex } from "@radix-ui/themes";
 import { FaBug } from "react-icons/fa";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const NavBar = () => {
   const links = [
@@ -7,27 +8,42 @@ const NavBar = () => {
     { label: "Issues", to: "/issues" },
   ];
 
+  const status = "authenticated";
+
   return (
-    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
-      <div>
-        <FaBug />
-      </div>
-      <ul className="flex space-x-6">
-        {links.map((link, index) => (
-          <li key={index}>
-            <NavLink
-              className={({ isActive }) =>
-                isActive
-                  ? "text-zinc-900"
-                  : "text-zinc-500 hover:text-zinc-800 transition-colors"
-              }
-              to={link.to}
-            >
-              {link.label}
-            </NavLink>
-          </li>
-        ))}
-      </ul>
+    <nav className="border-b mb-5 px-5 py-3">
+      <Container>
+        <Flex justify="between">
+          <Flex align="center" gap="3">
+            <Link to="/">
+              <FaBug />
+            </Link>
+            <ul className="flex space-x-6">
+              {links.map((link, index) => (
+                <li key={index}>
+                  <NavLink
+                    className={({ isActive }) =>
+                      isActive
+                        ? "text-zinc-900"
+                        : "text-zinc-500 hover:text-zinc-800 transition-colors"
+                    }
+                    to={link.to}
+                  >
+                    {link.label}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </Flex>
+          <Flex>
+            {" "}
+            <Box>
+              {status === "authenticated" && <Link to="/logout">Logout</Link>}
+              {status === "unauthenticated" && <Link to="/signin">Login</Link>}
+            </Box>
+          </Flex>
+        </Flex>
+      </Container>
     </nav>
   );
 };
