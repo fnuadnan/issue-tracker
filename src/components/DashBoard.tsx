8b83163128a354dbe2567a50240467f3761d66cb
Fnu Adnan <129648022+fnuadnan@users.noreply.github.com>
2024-06-20 23:16:26 -0500

    Lay out the dashboard

--- src/components/DashBoard.tsx
+++ src/components/DashBoard.tsx
@@ -1,3 +1,4 @@
+import { Flex, Grid } from "@radix-ui/themes";
 import useFetchIssueSummary from "../hooks/useFetchIssueSummary";
 import IssueChart from "./IssueChart";
 import IssueSummary from "./IssueSummary";
@@ -6,19 +7,21 @@ import LatestIssues from "./LatestIssues";
 const DashBoard = () => {
   const { summary } = useFetchIssueSummary();
   return (
-    <>
+    <Grid gap="5" columns={{ initial: "1", md: "2" }}>
+      <Flex gap="5" direction="column">
+        <IssueSummary
+          open={summary.open}
+          inProgress={summary.closed}
+          closed={summary.closed}
+        />
+        <IssueChart
+          open={summary.open}
+          inProgress={summary.closed}
+          closed={summary.closed}
+        />
+      </Flex>
       <LatestIssues />
-      <IssueSummary
-        open={summary.open}
-        inProgress={summary.closed}
-        closed={summary.closed}
-      />
-      <IssueChart
-        open={summary.open}
-        inProgress={summary.closed}
-        closed={summary.closed}
-      />
-    </>
+    </Grid>
   );
 };
 
