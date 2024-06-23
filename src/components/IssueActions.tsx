4a90daab2b5e98976dace7e082033f3879a52fdc
Fnu Adnan <129648022+fnuadnan@users.noreply.github.com>
2024-06-23 17:37:19 -0500

    Build the filter component

--- src/components/IssueActions.tsx
+++ src/components/IssueActions.tsx
@@ -1,13 +1,15 @@
-import { Button } from "@radix-ui/themes";
+import { Button, Flex } from "@radix-ui/themes";
 import { Link } from "react-router-dom";
+import IssueStatusFilter from "./IssueStatusFilter";
 
 const IssueActions = () => {
   return (
-    <div className="mb-5">
+    <Flex mb="5" justify="between">
+      <IssueStatusFilter />
       <Button>
         <Link to="new">New Issue</Link>
       </Button>
-    </div>
+    </Flex>
   );
 };
 
---
+++ src/components/IssueStatusFilter.tsx
@@ -0,0 +1,26 @@
+import { Select } from "@radix-ui/themes";
+
+type Status = "OPEN" | "CLOSED" | "IN_PROGRESS";
+const statuses: { label: string; value?: Status }[] = [
+  { label: "All" },
+  { label: "Open", value: "OPEN" },
+  { label: "Closed", value: "CLOSED" },
+  { label: "In Progress", value: "IN_PROGRESS" },
+];
+
+const IssueStatusFilter = () => {
+  return (
+    <Select.Root>
+      <Select.Trigger placeholder="Filter by status..." />
+      <Select.Content>
+        {statuses.map((status, index) => (
+          <Select.Item key={status.value || index} value={status.value || " "}>
+            {status.label}
+          </Select.Item>
+        ))}
+      </Select.Content>
+    </Select.Root>
+  );
+};
+
+export default IssueStatusFilter;
